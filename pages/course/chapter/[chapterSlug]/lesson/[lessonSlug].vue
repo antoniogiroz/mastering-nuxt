8b9da0aa8d8536e0906b0763035f39bf346a940e
Antonio Giroz <antonioluisgil@gmail.com>
2022-12-04 09:01:26 +0100

    feat: add nested dynamic routes for lessons

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Lesson</h2>
+    <p>This is a lesson</p>
+    <p>Chapter: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
