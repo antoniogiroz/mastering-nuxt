4fee30aa3c5f014f869a6efc129fe64f7cd803a5
Antonio Giroz <antonioluisgil@gmail.com>
2022-12-04 09:27:03 +0100

    feat: add mock course data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,8 +1,25 @@
+<script lang="ts" setup>
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    chapter => chapter.slug === route.params.chapterSlug,
+  )
+})
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    lesson => lesson.slug === route.params.lessonSlug,
+  )
+})
+</script>
+
 <template>
   <div>
     <h2>Lesson</h2>
     <p>This is a lesson</p>
-    <p>Chapter: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson: {{ $route.params.lessonSlug }}</p>
+    <p>Chapter: {{ chapter.title }}</p>
+    <p>Lesson: {{ lesson.title }}</p>
   </div>
 </template>
